c2fb9d99f9aca5ed1aa9aa2f4b47f164aefffe9b
Vijayasarathi06 <141705162+Vijayasarathi06@users.noreply.github.com>
2024-09-27 07:42:53 +0530

    Frontend projects,pratice

---
+++ task.py
@@ -0,0 +1,40 @@
+##Task(list,dict)
+##Task 1
+my_list=[1,2,3,4,5]
+my_list.reverse()
+print(my_list)
+##Task 2
+list1=[1,2,3,4,5]
+list2=[6,7,8,9,10]
+list3=list1+list2
+print(list3)
+##Task 3
+##Task 4
+lista=["a","b","c",]
+listb=["d","e","f"]
+listc=lista+listb
+print(listc)
+##Task 5
+##Task 6
+listd=['a','b','c']
+listd.remove('a')
+print(listd)
+##Task 7
+##liste=['a','b','c']
+##liste.add(55)
+##print(liste)
+##Task 10
+listf=["sarathi","suriya","seenu"]
+listf.clear()
+print(listf)
+##Excercise 1
+##Exercise 2
+listg=[1,2,3]
+listh=[4,5,6]
+listi=listg+listh
+print(listi)
+##Exercise 3
+dict1={"Subject":"History"}
+dict1.fromkeys("subject")
+print(dict1)
+
